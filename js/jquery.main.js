3e9031e1df49d097c599b942747e0f1ebe0ea63e
Sergey <kuzmenkos1986@gmail.com>
2015-11-03 22:10:57 +0200

    11

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -1,5 +1,37 @@
 $(function(){
+        var Slider = function (obj) {
 
+        //private properties
+        var _self = this,
+            _next = obj.find($('.swiper-button-next')),
+            _prev = obj.find($('.swiper-button-prev')),
+            _obj = obj;
+
+        //private methods
+        var _addEvents = function () {
+
+            },
+            _init = function () {
+                _addEvents();
+            };
+        var swiper = new Swiper(_obj, {
+            slidesPerView: 1,
+            autoplay: 5000,
+            nextButton: _next,
+            prevButton: _prev,
+            loop: true,
+            spaceBetween: 0
+        });
+        //public properties
+
+        //public methods
+
+
+        _init();
+    };
+    $('.swiper-container').each(function () {
+        Slider($(this));
+    });
 } );
 
 var Shablon = function (obj) {
@@ -31,6 +63,6 @@ Shablon.prototype = {
 
 $(window).on({
     load: function () {
-        
+
     }
 });
